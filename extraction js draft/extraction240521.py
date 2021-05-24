fddadac68065d15c4915c2b662f30e84029d67fb
benjaminchong99 <76833465+benjaminchong99@users.noreply.github.com>
2021-05-24 16:28:18 +0800

    Delete extraction240521.py

--- extraction js draft/extraction240521.py
+++
@@ -1,9 +0,0 @@
-import wikipedia
-import wikipediaapi
-
-enterKeyword = input("Enter keyword to search")
-wikipedia.search(enterKeyword)
-
-summarypg = wikipedia.page(enterKeyword).summary
-
-print(summarypg)
