7494507bb29a4ce4b3f6496e5114a4cbe76d4f58
Chong Pok Shun <pschong261199@hotmail.com>
2021-06-08 23:56:47 +0800

    2 full sentences and most images

--- extraction js draft/update 010621 onwards/newextraction.js
+++ extraction js draft/update 010621 onwards/newextraction.js
@@ -2,6 +2,7 @@ let optionsAvailable = null;
 let wordHistory = [];
 let pageID = null;
 
+
 function setup() {
 
     // START WITH USER INPUT    
@@ -12,10 +13,11 @@ function setup() {
     startSearch(termTosearch);
 }
 
+
 function startSearch(termTosearch) {
    /* 1. CONVERT TERMTOSEARCH TO URL READABLE
       2. LOAD JSON FOR DO YOU MEAN */
-    encodedTermtoSearch = encodeURI(termTosearch)
+    encodedTermtoSearch = encodeURIComponent(termTosearch)
 
     console.log('Querying: ' + termTosearch); // to show what youaresearching for
 
@@ -26,6 +28,7 @@ function startSearch(termTosearch) {
 }
 /* ***END OF setup() FUNCTION*** */
 
+
 // start of defining functions used in setup()
 function gotContent(data){ //FUNCTION REQ TO SHOW OPTIONS IN DYM
     console.log(data);
@@ -34,15 +37,15 @@ function gotContent(data){ //FUNCTION REQ TO SHOW OPTIONS IN DYM
 }
 // ***END OF gotContent()***
 
+
 function optionsmaybe(element, index){ // DEF REQ FOR LATER
     title = optionsAvailable[index]['title']
-    //snippet = optionsAvailable[index]['snippet']
-    //pageID = optionsAvailable[index]['pageid']
     optionName = `option_${index}`
 
     document.getElementById(optionName).innerHTML = index +":" + title + '<br>';
     
 }
+
 
 // SEARCH SELECTED TERM FROM OPTIONS AVAILABLE
 function goldenButton(){
@@ -52,13 +55,15 @@ function goldenButton(){
 
     pageID = selectedOption['pageid']
 
-    /** SECTION FOR SNIPPET */
+    /** SECTION FOR URL related works */
     document.getElementById('wordSearched').innerHTML = selectedOption['title']
-    description = selectedOption['snippet']
-    document.getElementById('contentDisplay').innerHTML = description + '...'
+    
+    encodedOption = encodeURIComponent(selectedOption['title']); //encode word into url readable format
+
+    /**SECTION FOR TWO SENTENCES */
+    twoSentences(encodedOption)
 
     /** SECTION FOR HYPERLINK */
-    encodedOption = encodeURI(selectedOption['title']);
     hyperlink = `https://en.wikipedia.org/wiki/${encodedOption}`;
     //document.getElementById('getLink').innerHTML = hyperlink;
     WikiLink(hyperlink) //get hyperlink
@@ -71,9 +76,12 @@ function goldenButton(){
     endpoint = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${encodedOption}&origin=*`;
 
     loadJSON(endpoint, gotImage, 'jsonp'); //load content
+
+    /** SECTION TO GET SUGGESTIONS*/
     getSuggestions() // get suggestions in the second box
 }
 /** END OF GOLDEN BUTTON FUNCTION */
+
 
 // defintiions of functions required in goldenButton() 
 function WikiLink(hyperlink) { //func req to get & show hyperlink
@@ -82,19 +90,37 @@ function WikiLink(hyperlink) { //func req to get & show hyperlink
     document.getElementById("getLink").innerHTML = result;
 }   
 
+
 function displayHistory(OptionTitle) {
 
     wordHistory.push(OptionTitle + ' ');
     document.getElementById('searchedWords').innerHTML = wordHistory; //get wordHistory
 }
 
-function gotImage(data){ // func req to get image
 
+function twoSentences(word){
+    const sentenceAPI = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=2&exlimit=1&titles=${word}&explaintext=1&formatversion=2&format=json`
+
+    console.log(sentenceAPI)
+    loadJSON(sentenceAPI, getTwoSentences, 'jsonp')    
+}
+
+
+function getTwoSentences(data) {
+    console.log(data)
+    finaltwoSentences = data.query.pages[0]['extract']
+    document.getElementById('contentDisplay').innerHTML = finaltwoSentences
+}
+
+
+function gotImage(data){ // func req to get image
     myJSON = JSON.stringify(data);
+    console.log(myJSON)
+    imageplease = ''
     startofURL = myJSON.search('https')
     endofURL = myJSON.search('.jpg')
     if (startofURL === -1 || endofURL === -1){
-        imageplease = ''
+
     } else{
         imageplease = myJSON.substring(startofURL, endofURL+4);
     }
@@ -102,6 +128,18 @@ function gotImage(data){ // func req to get image
     img = document.createElement('img'); 
     img.src = imageplease; 
 	document.getElementById('imghtml').src = imageplease;
+
+    if (imageplease === ''){
+        console.log("Find backup image")
+        checkTitleStart = myJSON.indexOf('"title":')
+        filterTitle = myJSON.substring(checkTitleStart+9, myJSON.length)
+        checkTitleEnd = filterTitle.indexOf('"')
+        checkTitle = filterTitle.substring(0,checkTitleEnd)
+        console.log(checkTitle)
+        
+        encodedWordAgain = encodeURIComponent(checkTitle)
+        backupImage(encodedWordAgain);
+    }
 }
 
 
@@ -111,6 +149,7 @@ function getSuggestions(){
 
     loadJSON(urlSuggestions, findSuggestions, 'jsonp'); //load full content of selected option
 }
+
 
 function findSuggestions(data){
     helloSuggestions = JSON.stringify(data);
@@ -129,6 +168,7 @@ function findSuggestions(data){
     slicedLinks.forEach(createSuggestions);        
 }
 /**End of findSuggestions(data) */
+
 
 // definitions of functions in findSuggestions(data)
 function descendingUniqueSort(toBeSorted) { 
@@ -154,27 +194,31 @@ function descendingUniqueSort(toBeSorted) {
     return itemCounts.map(function(itemCount) { return itemCount.key; });
  }
 
+
 function createSuggestions(element, index) { 
     //print each suggestions as option to choose
     suggestedID = `suggestion_${index}`
     document.getElementById(suggestedID).innerHTML = element;
 }   
 
-/**FUNCTION TO SEARCH THE SUGGESTED WORD */
-/** 1. GET THE WORD  
- * 2. GET THE SNIPPET OF THE WORD
-*/
 
+/**FUNCTION TO SEARCH THE SUGGESTED WORD */
 function suggestionButton(){ //Search the suggestion
     indexSuggestion = document.getElementById('selectSuggestion').options.selectedIndex;
     suggestionSelected = slicedLinks[indexSuggestion];
+    test = suggestionSelected.match(/\|/g)
+    
+    if (test === null) {
+
+    }else{
+        indextoRemove = suggestionSelected.indexOf('|');
+        suggestionSelected = suggestionSelected.substring(0,indextoRemove)
+    }
+
+    encodedSuggestion = encodeURIComponent(suggestionSelected)
     
     displayHistory(suggestionSelected) // update search history
-
-    encodedSuggestion = encodeURI(suggestionSelected)
-    let urlforSnip = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodedSuggestion}&format=json`
-    
-    loadJSON(urlforSnip, snipSuggestion, 'jsonp'); //to extract snippet
+    twoSentences(encodedSuggestion)
     
     imageSuggestion = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${encodedSuggestion}&origin=*`;
 
@@ -184,12 +228,25 @@ function suggestionButton(){ //Search the suggestion
     WikiLink(hyperlinkSuggestion) // to extract hyperlink
 }
 
-function snipSuggestion(data){ //get snippet of suggestion
-    collectSnippet = data.query.search[0]['snippet'];
-    document.getElementById('contentDisplay').innerHTML =collectSnippet + '...'
-}
-// if sinppet has '.' aka complete sentences, then snippet will end at the last '.' else, add '...'
-function completeSentence(content){
-    //if (content.match('.') is True) {
 
+function backupImage(encodedWordAgain){
+    console.log('Finding backup image')
+    sampleimageurl = `https://en.wikipedia.org/w/api.php?action=parse&page=${encodedWordAgain}&format=json`
+
+    loadJSON(sampleimageurl, getimg, 'jsonp')
+
+    function getimg(data){
+        myDump = JSON.stringify(data)
+
+        lolsStart = myDump.indexOf('//upload')
+        lolsEnd = myDump.search(/\.(png|jpg|JPEG|svg)\\" decoding/)
+        url = myDump.substring(lolsStart+2,lolsEnd+4)
+        console.log(lolsStart, lolsEnd)
+        console.log(url)
+        imageplease = `https://${url}`
+        img = document.createElement('img'); 
+        img.src = imageplease; 
+	    document.getElementById('imghtml').src = imageplease;
+    
+    }
 }
